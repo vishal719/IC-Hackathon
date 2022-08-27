b4fd52a2aaf006248c296177d9d2b98e2836aba1
VishalK <vkarmakar321@gmail.com>
2022-08-27 23:38:32 +0530

    profile created

--- app/src/main/java/com/example/ic_hackathon/MainActivity.java
+++ app/src/main/java/com/example/ic_hackathon/MainActivity.java
@@ -1,14 +1,88 @@
 package com.example.ic_hackathon;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.app.Activity;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+
+import com.example.ic_hackathon.Fragments.ContactsFragment;
+import com.example.ic_hackathon.Fragments.HomeFragment;
+import com.example.ic_hackathon.Fragments.MyProfileFragment;
+import com.example.ic_hackathon.databinding.ActivityMainBinding;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
+
+    public static void setWindowFlag(Activity activity, final int bits, boolean on) {
+        Window win = activity.getWindow();
+        WindowManager.LayoutParams winParams = win.getAttributes();
+        if (on) {
+            winParams.flags |= bits;
+        } else {
+            winParams.flags &= ~bits;
+        }
+        win.setAttributes(winParams);
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    public void hideStatus(){
+        /* To make the status bar transparent*/
+
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+
+        setWindowFlag(this, WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS, false);
+        getWindow().setStatusBarColor(Color.TRANSPARENT);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M)
+            hideStatus();
+
+        binding.bottomNavigation.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                Fragment fragment = null;
+                FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+
+                switch(item.getItemId())
+                {
+                    case R.id.homee:
+                        transaction.replace(R.id.Main_activity,new HomeFragment());
+                        break;
+
+                    case R.id.host:
+                        transaction.replace(R.id.Main_activity,new ContactsFragment());
+                        break;
+
+                    case R.id.profile:
+                        transaction.replace(R.id.Main_activity, (new MyProfileFragment()));
+                        break;
+
+                }
+
+                transaction.commit();
+
+                return true;
+            }
+        });
+        getSupportFragmentManager().beginTransaction().replace(R.id.Main_activity,new HomeFragment()).commit();
     }
 }
